d0dd4ef38473b9ff1f66fb8fd9da85a095553a30
Priyankarale <88052104+Priyankarale@users.noreply.github.com>
2021-08-24 14:06:55 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Content } from "./component/Content";
+import { Header } from "./component/Header";
+import "./App.scss";
+
+const App = () => {
+  return (
+    <>
+      <Header />
+      <Content />
+    </>
+  );
+};
+
+export default App;
---
+++ src/component/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export const Header = () => {
+  return (
+    <header className="header">
+      <nav>
+        <div className="logo">
+          <img src="" alt="To do list" />
+        </div>
+      </nav>
+    </header>
+  );
+};
